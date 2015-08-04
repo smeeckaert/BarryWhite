7715aa715fe2c5871206bbcd3c83078347f3a1d6
Martin Smeeckaert <smeeckaert@novius.fr>
2015-08-04 15:20:16 +0200

    initial commit foundation and build

---
+++ README.md
@@ -0,0 +1,5 @@
+# BarryWhite
+
+BarryWhite is a sass collection of usefull features built over foundation.
+
+[http://smeeckaert.github.io/BarryWhite/]
---
+++ config.rb
@@ -0,0 +1,26 @@
+#require 'zurb-foundation'
+#require 'compass_twitter_bootstrap'
+
+require 'sassy-math'
+require 'modular-scale'
+
+# Set this to the root of your project when deployed:
+
+http_path = ""
+sass_dir = "sass/"
+
+css_dir = "html/css/"
+css_path = "html/css/"
+
+images_dir = "html/img/"
+images_path = "html/img/"
+generated_images_dir = "html/img/"
+
+javascripts_dir = "html/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style  = :compressed
+sass_options = {:debug_info => false}
+line_comments = false
